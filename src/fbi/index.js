90280af8ea9a5dda67ed2fecb0563654cbaad935
第一名的小蝌蚪 <30694912@qq.com>
2020-06-08 01:20:04 +0800

    【小蝌蚪日记：通过console.log高仿FBI Warning】

---
+++ src/fbi/index.js
@@ -0,0 +1,21 @@
+console.log(`%c                                                                            
+                                                                            
+                                                                            
+                               %c FBI WARNING %c                                
+                                                                            
+                                                                            
+%c        Federal Law provides severe civil and criminal penalties for        
+        the unauthorized reproduction,distribution, or exhibition of        
+         copyrighted motion pictures (Title 17, United States Code,         
+        Sections 501 and 508). The Federal Bureau of Investigation          
+         investigates allegations of criminal copyright infringement        
+                 (Title 17, United States Code, Section 506).               
+                                                                            
+                                                                            
+                                                                            
+`,
+'background: #000; font-size: 18px; font-family: monospace',
+'background: #f33; font-size: 18px; font-family: monospace; color: #eee; text-shadow:0 0 1px #fff',
+'background: #000; font-size: 18px; font-family: monospace',
+'background: #000; font-size: 18px; font-family: monospace; color: #ddd; text-shadow:0 0 2px #fff'
+)
